e6606dd0e742ab24a9a08042a63aa66776af28f1
Georg Drees <tuxlifan@derseegrog.org>
2018-02-16 09:01:25 +0100

    Put a '__name__ == "__main__"' block

--- paperbackup.py
+++ paperbackup.py
@@ -68,151 +68,151 @@ def finish_page(pdf, canv, pageno):
     pdf.append(document.page(canv, paperformat=paperformat_obj,
                              fittosize=0, centered=0))
 
-# main code
+if __name__ == "__main__":
 
-if len(sys.argv) != 2:
-    raise RuntimeError('Usage {} FILENAME.asc'.format(sys.argv[0]))
+    if len(sys.argv) != 2:
+        raise RuntimeError('Usage {} FILENAME.asc'.format(sys.argv[0]))
 
-input_path = sys.argv[1]
-if not os.path.isfile(input_path):
-    raise RuntimeError('File {} not found'.format(input_path))
-just_filename = os.path.basename(input_path)
+    input_path = sys.argv[1]
+    if not os.path.isfile(input_path):
+        raise RuntimeError('File {} not found'.format(input_path))
+    just_filename = os.path.basename(input_path)
 
-with open(input_path) as inputfile:
-    ascdata = inputfile.read()
+    with open(input_path) as inputfile:
+        ascdata = inputfile.read()
 
-# only allow some harmless characters
-# this is much more strict than neccessary, but good enough for key files
-# you really need to forbid ^, NULL and anything that could upset enscript
-allowedchars = re.compile(r"^[A-Za-z0-9/=+:., #@!()\n-]*")
-allowedmatch = allowedchars.match(ascdata)
-if allowedmatch.group() != ascdata:
-    raise RuntimeError('Illegal char found at %d >%s<'
-                       % (len(allowedmatch.group()),
-                          ascdata[len(allowedmatch.group())]))
+    # only allow some harmless characters
+    # this is much more strict than neccessary, but good enough for key files
+    # you really need to forbid ^, NULL and anything that could upset enscript
+    allowedchars = re.compile(r"^[A-Za-z0-9/=+:., #@!()\n-]*")
+    allowedmatch = allowedchars.match(ascdata)
+    if allowedmatch.group() != ascdata:
+        raise RuntimeError('Illegal char found at %d >%s<'
+                           % (len(allowedmatch.group()),
+                              ascdata[len(allowedmatch.group())]))
 
-# split the ascdata into chunks of max_bytes_in_barcode size
-# each chunk begins with ^<sequence number><space>
-# this allows to easily put them back together in the correct order
-barcode_blocks = []
-chunkdata = "^1 "
-for char in list(ascdata):
-    if len(chunkdata)+1 > max_bytes_in_barcode:
-        # chunk is full -> create barcode from it
-        barcode_blocks.append(create_barcode(chunkdata))
-        chunkdata = "^" + str(len(barcode_blocks)+1) + " "
+    # split the ascdata into chunks of max_bytes_in_barcode size
+    # each chunk begins with ^<sequence number><space>
+    # this allows to easily put them back together in the correct order
+    barcode_blocks = []
+    chunkdata = "^1 "
+    for char in list(ascdata):
+        if len(chunkdata)+1 > max_bytes_in_barcode:
+            # chunk is full -> create barcode from it
+            barcode_blocks.append(create_barcode(chunkdata))
+            chunkdata = "^" + str(len(barcode_blocks)+1) + " "
 
-    chunkdata += char
+        chunkdata += char
 
-# handle the last, non filled chunk too
-barcode_blocks.append(create_barcode(chunkdata))
+    # handle the last, non filled chunk too
+    barcode_blocks.append(create_barcode(chunkdata))
 
-# init PyX
-unit.set(defaultunit="cm")
-pdf = document.document()
+    # init PyX
+    unit.set(defaultunit="cm")
+    pdf = document.document()
 
-# place barcodes on pages
-pgno = 0   # page number
-ppos = 0   # position id on page
-c = canvas.canvas()
-for bc in range(len(barcode_blocks)):
-    # page full?
-    if ppos >= barcodes_per_page:
-        finish_page(pdf, c, pgno)
-        c = canvas.canvas()
-        pgno += 1
-        ppos = 0
+    # place barcodes on pages
+    pgno = 0   # page number
+    ppos = 0   # position id on page
+    c = canvas.canvas()
+    for bc in range(len(barcode_blocks)):
+        # page full?
+        if ppos >= barcodes_per_page:
+            finish_page(pdf, c, pgno)
+            c = canvas.canvas()
+            pgno += 1
+            ppos = 0
 
-    c.text(barcode_x_positions[ppos] + text_x_offset,
-           barcode_y_positions[ppos] + text_y_offset,
-           "%s (%i/%i)" % (text.escapestring(just_filename),
-                           bc+1, len(barcode_blocks)))
-    c.insert(bitmap.bitmap(barcode_x_positions[ppos],
-                           barcode_y_positions[ppos],
-                           barcode_blocks[bc], height=barcode_height))
-    ppos += 1
+        c.text(barcode_x_positions[ppos] + text_x_offset,
+            barcode_y_positions[ppos] + text_y_offset,
+            "%s (%i/%i)" % (text.escapestring(just_filename),
+                            bc+1, len(barcode_blocks)))
+        c.insert(bitmap.bitmap(barcode_x_positions[ppos],
+                               barcode_y_positions[ppos],
+                               barcode_blocks[bc], height=barcode_height))
+        ppos += 1
 
-finish_page(pdf, c, pgno)
-pgno += 1
+    finish_page(pdf, c, pgno)
+    pgno += 1
 
-fd, temp_barcode_path = mkstemp('.pdf', 'qr_', '.')
-# will use pdf as the tmpfile has a .pdf suffix
-pdf.writetofile(temp_barcode_path)
+    fd, temp_barcode_path = mkstemp('.pdf', 'qr_', '.')
+    # will use pdf as the tmpfile has a .pdf suffix
+    pdf.writetofile(temp_barcode_path)
 
-# prepare plain text output
-fd, temp_text_path = mkstemp('.ps', 'text_', '.')
-input_file_modification = datetime.fromtimestamp(os.path.getmtime(input_path)).strftime("%Y-%m-%d %H:%M:%S")
+    # prepare plain text output
+    fd, temp_text_path = mkstemp('.ps', 'text_', '.')
+    input_file_modification = datetime.fromtimestamp(os.path.getmtime(input_path)).strftime("%Y-%m-%d %H:%M:%S")
 
-# split lines on plaintext_maxlinechars - ( checksum_size + separator size)
-splitat=plaintext_maxlinechars - 8
-splitlines=[]
-for line in ascdata.splitlines():
-    while len(line) > splitat:
-        splitlines.append(line[:splitat])
-        # add a ^ at the beginning of the broken line to mark the linebreak
-        line="^"+line[splitat:]
-    splitlines.append(line)
+    # split lines on plaintext_maxlinechars - ( checksum_size + separator size)
+    splitat=plaintext_maxlinechars - 8
+    splitlines=[]
+    for line in ascdata.splitlines():
+        while len(line) > splitat:
+            splitlines.append(line[:splitat])
+            # add a ^ at the beginning of the broken line to mark the linebreak
+            line="^"+line[splitat:]
+        splitlines.append(line)
 
-# add checksums to each line
-chksumlines=[]
-for line in splitlines:
-    # remove the linebreak marks for checksumming
-    if len(line) > 1 and line[0] == "^":
-        sumon=line[1:]
-    else:
-        sumon=line
+    # add checksums to each line
+    chksumlines=[]
+    for line in splitlines:
+        # remove the linebreak marks for checksumming
+        if len(line) > 1 and line[0] == "^":
+            sumon=line[1:]
+        else:
+            sumon=line
 
-    # use the first 6 bytes of MD5 as checksum
-    chksum = hashlib.md5(sumon.encode('utf-8')).hexdigest()[:6]
+        # use the first 6 bytes of MD5 as checksum
+        chksum = hashlib.md5(sumon.encode('utf-8')).hexdigest()[:6]
 
-    # add the checksum right-justified to the line
-    line+=" "*(splitat-len(line))
-    line+=" |"+chksum
+        # add the checksum right-justified to the line
+        line+=" "*(splitat-len(line))
+        line+=" |"+chksum
 
-    chksumlines.append(line)
+        chksumlines.append(line)
 
-# add some documentation around the plaintest
-outlines=[]
-coldoc=" "*splitat
-coldoc+=" | MD5"
-outlines.append(coldoc)
-outlines.extend(chksumlines)
-outlines.append("")
-outlines.append("")
-outlines.append("")
-outlines.append("--")
-outlines.append("Created with paperbackup.py")
-outlines.append("See https://github.com/intra2net/paperbackup/ for instructions")
+    # add some documentation around the plaintest
+    outlines=[]
+    coldoc=" "*splitat
+    coldoc+=" | MD5"
+    outlines.append(coldoc)
+    outlines.extend(chksumlines)
+    outlines.append("")
+    outlines.append("")
+    outlines.append("")
+    outlines.append("--")
+    outlines.append("Created with paperbackup.py")
+    outlines.append("See https://github.com/intra2net/paperbackup/ for instructions")
 
-# use "enscript" to create postscript with the plaintext
-p = subprocess.Popen(
-        ["enscript", "-p"+temp_text_path, "-f", "Courier12",
-            "-M" + paperformat_str, "--header",
-            just_filename + "|" + input_file_modification + "|Page $%"],
-        stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    # use "enscript" to create postscript with the plaintext
+    p = subprocess.Popen(
+            ["enscript", "-p"+temp_text_path, "-f", "Courier12",
+                "-M" + paperformat_str, "--header",
+                just_filename + "|" + input_file_modification + "|Page $%"],
+            stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 
-# send the text to enscript
-for line in outlines:
-    p.stdin.write(line.encode('utf-8'))
-    p.stdin.write(os.linesep.encode('utf-8'))
+    # send the text to enscript
+    for line in outlines:
+        p.stdin.write(line.encode('utf-8'))
+        p.stdin.write(os.linesep.encode('utf-8'))
 
-p.communicate()[0]
-p.stdin.close()
+    p.communicate()[0]
+    p.stdin.close()
 
-if p.returncode != 0:
-    raise RuntimeError('error calling enscript')
+    if p.returncode != 0:
+        raise RuntimeError('error calling enscript')
 
-# combine both files with ghostscript
+    # combine both files with ghostscript
 
-ret = subprocess.call(["gs", "-dBATCH", "-dNOPAUSE", "-q", "-sDEVICE=pdfwrite",
-                       "-sOutputFile=" + just_filename + ".pdf",
-                       temp_barcode_path, temp_text_path])
-if ret != 0:
-    raise RuntimeError('error calling ghostscript')
+    ret = subprocess.call(["gs", "-dBATCH", "-dNOPAUSE", "-q", "-sDEVICE=pdfwrite",
+                        "-sOutputFile=" + just_filename + ".pdf",
+                        temp_barcode_path, temp_text_path])
+    if ret != 0:
+        raise RuntimeError('error calling ghostscript')
 
-# using enscript and ghostscript to create the plaintext output is a hack,
-# using PyX and LaTeX would be more elegant. But I could not find an easy
-# solution to flow the text over several pages with PyX.
+    # using enscript and ghostscript to create the plaintext output is a hack,
+    # using PyX and LaTeX would be more elegant. But I could not find an easy
+    # solution to flow the text over several pages with PyX.
 
-os.remove(temp_text_path)
-os.remove(temp_barcode_path)
+    os.remove(temp_text_path)
+    os.remove(temp_barcode_path)
