760ee915c66825623e876121a828e387bee231b5
Fedor Kotov <fedorkotov@gmail.com>
2018-07-08 13:32:56 +0300

    Changed default encryption tool to gpg2 with fallback to gpg if gpg2 is unavailable

--- gpg-paperbackup.sh
+++ gpg-paperbackup.sh
@@ -4,15 +4,27 @@
 # and prepares paperbackup PDF of encrypted version
 #
 # USAGE: gpg-paperbackup.sh plaintext_fpath output_fpath
-#   where plaintext_fpath is plaintext file to encode (can be text or binary)
-#         encrypted and base64 encoded version of plaintext will be written
-#         to output_fpath
 #
-#   output_encrypted_path will then be passed to paperbackup.py and 
+#   where plaintext_fpath is plaintext file to encode (can be text or binary).
+#   encrypted and base64 encoded version of plaintext will be written
+#   to output_fpath.
+#   The script uses gpg2 for symmetric encryption and falls back to gpg
+#   if gpg2 is not available.
+#
+#   output_encrypted_path will then be passed to paperbackup.py and
 #   result written to output_encrypted_path.pdf
 
 PAPERBACKUPPATH="$(readlink -f $(dirname $0))"
-gpg --symmetric -o- "$1" | base64 > "$2"
+
+if ! GPGPATH=$(command -v gpg2) ; then
+  if ! GPGPATH=$(command -v gpg) ; then
+    echo "ERROR: gpg and gpg2 commands not found"
+    exit 1
+  fi
+fi
+echo "${GPGPATH} will be used for encryption"
+
+${GPGPATH} --symmetric -o- "$1" | base64 > "$2"
 ${PAPERBACKUPPATH}/paperbackup.py "$2"
 ${PAPERBACKUPPATH}/paperbackup-verify.sh "${2}.pdf"
 
--- paperrestore-gpg.sh
+++ paperrestore-gpg.sh
@@ -5,7 +5,20 @@
 # USAGE: paperrestore-gpg.sh input_fpath output_fpath
 #   where input_fpath is path to PDF with scanned paper backup
 #   previously created with gpg-paperbackup.sh
-#   Decrypted plaintext will be written to output_fpath
+#   Decrypted plaintext will be written to output_fpath.
+#   The script uses gpg2 for decryption and falls back to gpg
+#   if gpg2 is not available.
+
 
 PAPERBACKUPPATH="$(readlink -f $(dirname $0))"
-${PAPERBACKUPPATH}/paperrestore.sh "$1" | base64 --decode | gpg -d > "$2"
+
+if ! GPGPATH=$(command -v gpg2) ; then
+  if ! GPGPATH=$(command -v gpg) ; then
+    echo "ERROR: gpg and gpg2 commands not found"
+    exit 1
+  fi
+fi
+echo "${GPGPATH} will be used for encryption"
+
+
+${PAPERBACKUPPATH}/paperrestore.sh "$1" | base64 --decode | ${GPGPATH} -d > "$2"
