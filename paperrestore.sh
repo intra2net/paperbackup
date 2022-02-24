7a69704d27c3939fc2878319c0e63582dd8d769a
Todd A. Jacobs <385462+todd-a-jacobs@users.noreply.github.com>
2022-02-23 20:37:27 -0500

    don't rely on a hard-coded location for zbarimg
    - Use `which` to check the PATH for zbarimg, rather than hard-coding its
      location.
    - Use whichever zbarimg is in the PATH, rather than defining the
      executable's fully-qualified PATH.
    - If zbarimg is missing, the error message now identifies the PATH as
      the problem, rather than pointing to a (possibly) incorrect location
      for the executable.

--- paperrestore.sh
+++ paperrestore.sh
@@ -21,10 +21,10 @@ if [ ! -f "$SCANNEDFILE" ]; then
     exit 1
 fi
 
-if [ ! -x "/usr/bin/zbarimg" ]; then
-    echo "/usr/bin/zbarimg missing" > /dev/stderr
+which zbarimg > /dev/null 2>&1 || {
+    echo "zbarimg not found in PATH" > /dev/stderr
     exit 2
-fi
+}
 
 # zbarimg ends each scanned code with a newline
 
@@ -32,7 +32,7 @@ fi
 # so convert that to \0<number><space>, so sort can sort on that
 # then remove all \n\0<number><space> so we get the originial without newlines added
 
-/usr/bin/zbarimg --raw -Sdisable -Sqrcode.enable "$SCANNEDFILE" \
+zbarimg --raw -Sdisable -Sqrcode.enable "$SCANNEDFILE" \
     | sed -e "s/\^/\x0/g" \
     | sort -z -n \
     | sed ':a;N;$!ba;s/\n\x0[0-9]* //g;s/\x0[0-9]* //g;s/\n\x0//g'
